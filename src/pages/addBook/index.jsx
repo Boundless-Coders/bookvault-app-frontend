7b03c9fd3071f1c0af13b609cb97517e9349a5f9
Believe Kwamitse <believeserwaa80@gmail.com>
2024-10-07 15:14:24 +0000

    first commit

---
+++ src/pages/addBook/index.jsx
@@ -0,0 +1,18 @@
+import Footer from "../../components/Footer";
+import Navbar from "../../components/Navbar";
+
+const AddBook = () => {
+  return (
+    <div>
+        <Navbar />
+        <span>Add a New Book</span>
+        <form >
+            <input />
+            <button>Sumbit</button>
+        </form>
+        <Footer />
+    </div>
+  )
+}
+
+export default AddBook;
